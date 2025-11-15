a15fe09d801ea0d81ccd108ae81c5cd7f1d88d0a
dquintor <dany.quintorios@gmail.com>
2025-11-15 18:37:06 -0500

    Validacion de numeros enteros en el menu de seleccion

--- utils.py
+++ utils.py
@@ -1,1 +1,10 @@
-
+def validar_entero_menu(menu_texto, mensaje, minimo=None, maximo=None):
+    while True:
+        print(menu_texto)
+        try:
+            valor = int(input(mensaje).strip())
+            if minimo is not None and valor < minimo or maximo is not None and valor > maximo:
+                raise ValueError
+            return valor
+        except ValueError:
+            print("Opción inválida. Ingrese un valor disponible en el menú.\n")
